dfd8f4cccc009ff69925c5912a4611107ea4135a
Lucas97111 <redapples2007@gmail.com>
2026-03-16 16:05:18 +1100

    Started UI work

---
+++ Assets/Scripts/UI_scripts/InGameUiHandle.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameUiHandler : MonoBehaviour
+{
+
+    public Image heart1;
+    public Image heart2;
+    public Image heart3;
+
+  
+    public Sprite FullHeartImage;
+
+    public Sprite EmptyHeartImage;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Application.targetFrameRate = 1;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RemoveHeart();
+        print("frame update");
+    }
+
+
+    public void RemoveHeart()
+    {
+        if (heart2.sprite == EmptyHeartImage)
+        {
+            heart1.sprite = EmptyHeartImage;
+        }
+        
+        if (heart3.sprite == EmptyHeartImage)
+        {
+            heart2.sprite = EmptyHeartImage;
+        }
+
+        heart3.sprite = EmptyHeartImage;
+       
+
+        
+    }
+}
