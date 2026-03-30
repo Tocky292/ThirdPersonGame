1127587d7f9118b036a8dc30bbfbb7a8b10ca359
Lucas97111 <redapples2007@gmail.com>
2026-03-30 12:26:54 +1100

    Updated UI to go to final scene

--- Assets/Scripts/UIScripts/MainMenuUIHandler.cs
+++ Assets/Scripts/UIScripts/MainMenuUIHandler.cs
@@ -9,7 +9,7 @@ public class MainMenuUIHandler : MonoBehaviour
     public void StartGame()
     {
         print("Start game");
-        SceneManager.LoadScene("Ethan_Scene");
+        SceneManager.LoadScene("Final_Scene");
     }
 
     public void QuitGame()
